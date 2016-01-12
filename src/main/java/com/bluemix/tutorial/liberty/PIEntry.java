9e10f19a8c3063731259c0e8947285cb53d67798
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2016-01-12 11:56:10 +0100

    watson 2.5 sdk fixes the Double/double issue for persistence of Profile - no need for workaround anymore

--- src/main/java/com/bluemix/tutorial/liberty/PIEntry.java
+++ src/main/java/com/bluemix/tutorial/liberty/PIEntry.java
@@ -1,14 +1,12 @@
 package com.bluemix.tutorial.liberty;
 
 import java.util.Date;
-import java.util.List;
 
 import org.ektorp.support.CouchDbDocument;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.ibm.watson.developer_cloud.personality_insights.v2.model.Profile;
-import com.ibm.watson.developer_cloud.personality_insights.v2.model.Trait;
 
 @SuppressWarnings("serial")
 @JsonInclude(Include.NON_EMPTY)
@@ -54,33 +52,6 @@ public class PIEntry extends CouchDbDocument {
 
   public void setProfile(Profile profile) {
     this.profile = profile;
-    fixTrait(profile.getTree());
   }
 
-  /**
-   * Circumvent an issue when the "Trait" class currently has "double" return
-   * types for some "Double" objects that might be null. It throws NPE when
-   * unboxing the value during serialization by Jackson as Jackson will call the
-   * getter.
-   * 
-   * @see https://github.com/watson-developer-cloud/java-sdk/issues/151
-   */
-  private static void fixTrait(Trait trait) {
-    try {
-      trait.getRawSamplingError();
-    } catch (NullPointerException e) {
-      trait.setRawSamplingError(0);
-    }
-    try {
-      trait.getRawScore();
-    } catch (NullPointerException e) {
-      trait.setRawScore(0);
-    }
-    List<Trait> children = trait.getChildren();
-    if (children != null) {
-      for (Trait child : children) {
-        fixTrait(child);
-      }
-    }
-  }
 }
